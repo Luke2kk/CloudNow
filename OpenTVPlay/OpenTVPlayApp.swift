ee577ec90cf2bf829cf637da7190efef8d9d8658
Owen Selles <owenselles@icloud.com>
2026-04-11 12:34:06 +0200

    Initial Commit

---
+++ OpenTVPlay/OpenTVPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTVPlayApp.swift
+//  OpenTVPlay
+//
+//  Created by Owen Selles on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenTVPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
